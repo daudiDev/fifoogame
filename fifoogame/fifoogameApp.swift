2b84416481da144585cbc6d0bb05a1631a88ca5f
Daudi Sagala <daudi.sagala@gmail.com>
2026-08-18 14:31:17 -0400

    Initial Commit

---
+++ fifoogame/fifoogameApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifoogameApp.swift
+//  fifoogame
+//
+//  Created by Daudi Sagala on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct fifoogameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
